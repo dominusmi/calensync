43c506d5336ba66da1c41469ac487e775ef30005
dominusmi <barp.edoardo@gmail.com>
2023-12-06 22:14:27 +0100

    Fix footer links

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { PUBLIC_URL } from '../utils/const';
 
 const Footer: React.FC = () => {
   return (
     <footer className="container mt-5 footer bg-light p-3">
       <div className='row'>
-        <a href="/tos">Terms of Use</a>
+        <a href={`${PUBLIC_URL}/tos`}>Terms of Use</a>
       </div>
       <div className='row'>
-        <a href="/privacy">Privacy Policy</a>
+        <a href={`${PUBLIC_URL}/privacy`}>Privacy Policy</a>
       </div>
       <div className='row'>
         <p>Calensync © 2023</p>
