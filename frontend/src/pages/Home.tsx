2a9bc66685d5be449cf70111be5e752f3c534069
dominusmi <barp.edoardo@gmail.com>
2023-12-06 09:02:22 +0100

    Home page design

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -25,15 +25,16 @@ const Home: React.FC = () => {
 
             <div className='content'>
                 <div className='hero'>
-                    <div className="container col-xxl-8 px-4 py-5">
+                    <div className="container col-xxl-8 py-5">
                         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                             <div className="col-10 col-sm-8 col-lg-6">
                                 <img src="hero.gif" className="d-block mx-lg-auto img-fluid hero-gif" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
                             </div>
                             <div className="col-lg-6">
-                                <h1 className="display-5 fw-bold lh-1 mb-3">No more conflict of event</h1>
+                                <h1 className="display-5 fw-bold lh-1 mb-3">No more calendar event conflicts.</h1>
                                 <p className="lead">
-                                    Sync all your Google calendars together and save yourself trouble.
+                                    Save yourself the pain of rescheduling.
+                                    Sync all your Google calendars together and save yourself trouble of having to reschedule appointments.
                                     Make sure no one ever books a meeting when you're already busy.
                                 </p>
                                 <div className="d-grid gap-2 d-md-flex justify-content-md-start mt-4">
@@ -44,15 +45,21 @@ const Home: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className='container'>
-                    <div className="row g-4 my-5 py-5 row-cols-1 row-cols-lg-3">
+                <div className='container col-xxl-8 '>
+                    <div className="row g-4 mt-5 mb-3 pt-5 pb-0 row-cols-1 row-cols-lg-2">
                         <div className="feature col">
                             <h2>Unlimited Calendars</h2>
-                            <p className='lead'>No limit on the number of accounts and calendars you want to sync together</p>
+                            <p className='lead'>No limit on the number of accounts and calendars you want to sync together. Gotta catch them all!</p>
                         </div>
                         <div className="feature col">
                             <h2>Synchronized in seconds</h2>
-                            <p className='lead'>When an event is created or modified, it is updated on all calendars in a matter of seconds!</p>
+                            <p className='lead'>When an event is created or modified, it is updated on all calendars in seconds.</p>
+                        </div>
+                        </div>
+                        <div className="row g-4 mt-3 mb-5 pt-3 pb-5 row-cols-1 row-cols-lg-2">
+                        <div className="feature col">
+                            <h2>Privacy first</h2>
+                            <p className='lead'>Information about events is not synchronized between calendars - instead, a blocker event is created in order to protect your privacy.</p>
                         </div>
                         <div className="feature col">
                             <h2>Ready to go in 30 Seconds</h2>
