6a1c8fe8d90ba46006da7af4e549bf27c1737576
dominusmi <barp.edoardo@gmail.com>
2023-12-08 12:25:19 +0100

    Add a way to directly verify the session in the layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,23 +2,26 @@ import React, { ReactNode, useEffect, useState } from 'react';
 import Footer from './Footer';
 import NavBar from './Navbar';
 import Toast from './Toast';
-import { consumeMessages } from '../utils/common';
+import { MessageKind, consumeMessages, setMessage } from '../utils/common';
+import verify_session_id, { VerifySession } from '../utils/session';
+import { PUBLIC_URL } from '../utils/const';
 
 interface LayoutProps {
     children: ReactNode;
+    verify_session?: boolean;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, verify_session = true }) => {
     const [toastReady, setToastReady] = useState(false);
 
     const handleToastReady = () => {
-      setToastReady(true);
+        setToastReady(true);
     };
 
     useEffect(() => {
         // Your function to be triggered after the component is ready
         consumeMessages();
-      }, [toastReady]);
+    }, [toastReady]);
 
     return (
         <div className='App bg-light'>
