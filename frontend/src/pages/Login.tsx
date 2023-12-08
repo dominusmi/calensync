5b3c45da1c3587e01c0587f3c26d4250ddb79e9e
dominusmi <barp.edoardo@gmail.com>
2023-12-08 12:28:46 +0100

    Add pre-signup form until launch

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginCard from '../components/LoginCard';
 import Layout from '../components/Layout';
+import { ENV } from '../utils/const';
+import LoadingOverlay from '../components/LoadingOverlay';
+
 
 const Login: React.FC = () => {
+  const [isProduction, setIsProduction] = useState<boolean>(ENV == 'production');
+
   return (
-    <Layout>
-      <div className="container centered">
-        <LoginCard />
+    <Layout verify_session={false}>
+      <div className="container">
+        { !isProduction &&
+          <div className='centered'>
+            <LoginCard />
+          </div>
+        }
+        {
+          isProduction &&
+          <div className='mt-4'>
+            <h3>Stay tuned.</h3>
+            <iframe src="https://tally.so/embed/mZ2K5e?alignLeft=1&hideTitle=1&transparentBackground=1" width={"100%"} height={"100%"} style={{ minHeight: "500px" }}></iframe>
+          </div>
+        }
       </div>
     </Layout>
   );
