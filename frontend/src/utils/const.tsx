f4fbc0867b1a968840ead27baea81cccf1a27a5b
dominusmi <barp.edoardo@gmail.com>
2023-12-08 12:29:10 +0100

    utilities

--- frontend/src/utils/const.tsx
+++ frontend/src/utils/const.tsx
@@ -1,5 +1,6 @@
 export const API = process.env.REACT_APP_API_URL || ''; // Access the environment variable
 export const PUBLIC_URL = process.env.PUBLIC_URL || '';
 export const PADDLE_CLIENT_TOKEN = process.env.REACT_APP_PADDLE_CLIENT_TOKEN!;
+export const ENV = process.env.REACT_APP_ENV!;
 
 export default API
--- frontend/src/utils/session.tsx
+++ frontend/src/utils/session.tsx
@@ -1,13 +1,19 @@
-import API from "./const";
+import { MessageKind, setMessage } from "./common";
+import API, { PUBLIC_URL } from "./const";
 
 export enum VerifySession {
-    OK = 0,
-    INVALID = 1,
-    TOS = 2,
-    LOGIN = 3
-  }
+  INVALID = 1,
+  TOS = 2,
+  LOGIN = 3
+}
 
-export async function verify_session_id(): Promise<VerifySession> {
+export interface User {
+  customer_id: string;
+  date_created: Date;
+  subscription_id: string | null;
+}
+
+export async function verify_session_id(): Promise<VerifySession | User> {
   const session_id = get_session_id();
 
   if (session_id == null) {
@@ -22,18 +28,20 @@ export async function verify_session_id(): Promise<VerifySession> {
       },
     });
 
-    console.log(response)
-    if(response.status === 309){
+    if (response.status === 309) {
       return VerifySession.TOS;
     }
     else if (!response.ok) {
-      if(response.status == 403){
+      if (response.status == 403) {
         localStorage.removeItem("session-id")
         return VerifySession.LOGIN;
       }
       return VerifySession.INVALID;
     }
-    return VerifySession.OK;
+    let user = await response.json();
+    user.date_created = new Date(user.date_created);
+    return user;
+
   } catch (error) {
     return VerifySession.INVALID;
   }
@@ -42,6 +50,18 @@ export async function verify_session_id(): Promise<VerifySession> {
 export function get_session_id() {
   // Implement the logic to retrieve the session ID from localStorage or wherever it is stored
   return localStorage.getItem("session-id")
+}
+
+
+export const getLoggedUser: () => Promise<User> = async () => {
+  const result = await verify_session_id();
+  if (result == VerifySession.TOS) {
+    setMessage("Must accept Terms of Use", MessageKind.Info)
+    window.location.href = `${PUBLIC_URL}/tos`;
+  } else if (result == VerifySession.LOGIN || result === VerifySession.INVALID) {
+    window.location.href = `${PUBLIC_URL}/login`;
+  }
+  return result as User;
 }
 
 export default verify_session_id;
