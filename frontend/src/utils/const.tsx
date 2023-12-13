e4a59c7035cfdc65fdeb905c7bd419bd9fdd9c0e
dominusmi <barp.edoardo@gmail.com>
2023-12-13 11:20:33 +0100

    Add PADDLE_PRICING const

--- frontend/src/utils/const.tsx
+++ frontend/src/utils/const.tsx
@@ -2,5 +2,6 @@ export const API = process.env.REACT_APP_API_URL || ''; // Access the environmen
 export const PUBLIC_URL = process.env.PUBLIC_URL || '';
 export const PADDLE_CLIENT_TOKEN = process.env.REACT_APP_PADDLE_CLIENT_TOKEN!;
 export const ENV = process.env.REACT_APP_ENV!;
+export const PADDLE_PRICING = process.env.REACT_APP_PADDLE_PRICING;
 
 export default API
