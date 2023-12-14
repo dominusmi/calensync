82ea2412673d763c0322844c818b7330c8ff7d58
dominusmi <barp.edoardo@gmail.com>
2023-12-14 15:34:23 +0100

    Add checks to no created or updated

--- backend/lib/calensync/tests/test_dataclasse.py
+++ backend/lib/calensync/tests/test_dataclasse.py
@@ -9,3 +9,25 @@ def test_parse_event():
 
     events = GoogleEvent.parse_event_list_response(data)
     assert len(events) > 1
+
+
+def test_no_created_attribute():
+    with open("list_events.json") as f:
+        data = json.load(f)
+
+    before = len(data["items"])
+    data["items"][0].pop("created")
+    events = GoogleEvent.parse_event_list_response(data)
+    after = len(events)
+    assert before == after
+
+
+def test_no_updated_attribute():
+    with open("list_events.json") as f:
+        data = json.load(f)
+
+    before = len(data["items"])
+    data["items"][0].pop("updated")
+    events = GoogleEvent.parse_event_list_response(data)
+    after = len(events)
+    assert before == after
