2ce01c2dc2f5d738265b5f9eafd51196633b543a
dominusmi <barp.edoardo@gmail.com>
2023-12-19 14:00:36 +0100

    Fix Navbar formatting

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,67 +1,55 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import { optimisticIsConnected, logout } from '../utils/session';
+import { optimisticIsConnected, logout, getLocalSession } from '../utils/session';
 import { PUBLIC_URL } from '../utils/const';
 import { useEffect, useState } from 'react';
 
-function NavBar() {
+const NavBar: React.FC<{ verify_session?: boolean }> = ({ verify_session = true }) => {
   const [isConnected, setIsConnected] = useState<boolean>(false);
 
   useEffect(() => {
-    optimisticIsConnected().then((loggedIn) => setIsConnected(loggedIn))
+    if (verify_session) {
+      optimisticIsConnected().then((loggedIn) => setIsConnected(loggedIn))
+    } else {
+      setIsConnected(getLocalSession() !== null);
+    }
   }, [])
 
-  function handleLogout(){
+  function handleLogout() {
     logout().then(() => window.location.href = `/login`)
     localStorage.removeItem('session-id');
   }
 
   return (
-    <div>
-      {isConnected &&
-        <Navbar expand="md" className="hero py-2 hero-navbar d-flex justify-content-center px-3">
-          <Container className='container col-xxl-8'>
-            <Navbar.Brand href={`${PUBLIC_URL}/`}> <img
-              src={`${PUBLIC_URL}/logo.png`}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Calensync logo"
-            /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto lead">
+    <Navbar expand="md" className="hero py-2 hero-navbar d-flex justify-content-center">
+      <Container className='container px-xxl-0 col-xxl-8'>
+        <Navbar.Brand href={`${PUBLIC_URL}/`}> <img
+          src={`${PUBLIC_URL}/logo.png`}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Calensync logo"
+        /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {isConnected &&
+            <Nav className="me-auto lead">
               <Nav.Link href={`${PUBLIC_URL}/`}>Home</Nav.Link>
-                <Nav.Link href={`${PUBLIC_URL}/dashboard`}>Dashboard</Nav.Link>
-                <Nav.Link href={`${PUBLIC_URL}/plan`}>Plan</Nav.Link>
-                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      }
-      {!isConnected &&
-        <Navbar expand="md" className="hero py-2 hero-navbar d-flex justify-content-center px-3">
-          <Container className='p-0'>
-            <Navbar.Brand href={`${PUBLIC_URL}/`}> <img
-              src={`${PUBLIC_URL}/logo.png`}
-              width="40"
-              height="40"
-              className="d-inline-block align-top"
-              alt="Calensync logo"
-            /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto lead">
-                <Nav.Link href={`${PUBLIC_URL}/`}>Home</Nav.Link>
-                <Nav.Link href={`${PUBLIC_URL}/login`}>Login</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      }
-    </div>
+              <Nav.Link href={`${PUBLIC_URL}/dashboard`}>Dashboard</Nav.Link>
+              <Nav.Link href={`${PUBLIC_URL}/plan`}>Plan</Nav.Link>
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            </Nav>
+          }
+          {!isConnected &&
+            <Nav className="me-auto lead">
+              <Nav.Link href={`${PUBLIC_URL}/`}>Home</Nav.Link>
+              <Nav.Link href={`${PUBLIC_URL}/login`}>Login</Nav.Link>
+            </Nav>
+          }
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
