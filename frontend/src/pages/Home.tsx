c80686c888775fbe400bffd50a9fef132f7bf32d
dominusmi <barp.edoardo@gmail.com>
2023-12-21 13:49:05 +0100

    Minor translation fix

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -89,7 +89,7 @@ const Home: React.FC = () => {
             </div>
             <div className='container'>
                 <div className='col-xxl-8 col-12 px-4 card mt-4 pt-4 pb-2 mx-auto'>
-                    <a className='block-link' href={`/blog/sync-multiple-google-calendars`}>
+                    <a className='block-link' href="/blog/sync-multiple-google-calendars">
                         <p className='text-muted small p-0 m-0'>Blog</p>
                         <h2>{t("blog_list.sync_google_calendars.title")}</h2>
                         <p className='text-muted'>
@@ -100,7 +100,7 @@ const Home: React.FC = () => {
             </div>
             <div className='container'>
                 <div className='col-xxl-8 col-12 px-4 card mt-4 pt-4 pb-2 mx-auto'>
-                    <a className='block-link' href={`/blog/avoid-calendly-conflicts`}>
+                    <a className='block-link' href="blog/avoid-calendly-conflicts">
                         <p className='text-muted small p-0 m-0'>Blog</p>
                         <h2>{t("blog_list.avoid_calendly_conflicts.title")}</h2>
                         <p className='text-muted'>
