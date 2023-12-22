af15bf1f80bc35022865a69dd5b9d2e3e962e322
dominusmi <barp.edoardo@gmail.com>
2023-12-22 10:28:56 +0100

    Improve footer layout and add second column. Add for freelancers

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,22 +2,43 @@ import React from 'react';
 import { PUBLIC_URL } from '../utils/const';
 import { useTranslation } from 'react-i18next';
 
-const Footer: React.FC = () => {
+const Footer: React.FC<{ onlyRequired?: boolean }> = ({ onlyRequired = false }) => {
   const { t } = useTranslation();
 
+
   return (
-    <footer className="container mt-5 footer bg-light p-3 col-xxl-8  ">
-      <div className='row'>
-        <a href={`${PUBLIC_URL}/tos`}>{t("footer.tos")}</a>
-      </div>
-      <div className='row'>
-        <a href={`${PUBLIC_URL}/privacy`}>{t("footer.privacy")}</a>
-      </div>
-      <div className='row'>
-        <a href={`${PUBLIC_URL}/google-privacy`}>{t("footer.google_disclosure")}</a>
-      </div>
-      <div className='row'>
-        <p>Calensync © 2023</p>
+    <footer className="container content mt-5 footer col-xxl-8" >
+      <div className="row col-xxl-8 d-flex justify-content-between">
+        <div className="col-12 col-sm-6 col-xxl-4 mt-sm-0 mt-3">
+          <div className="row">
+            <a href={`${PUBLIC_URL}/tos`}>{t("footer.tos")}</a>
+          </div>
+          <div className="row">
+            <a href={`${PUBLIC_URL}/privacy`}>{t("footer.privacy")}</a>
+          </div>
+          <div className="row">
+            <a href={`${PUBLIC_URL}/google-privacy`}>{t("footer.google_disclosure")}</a>
+          </div>
+          <div className="row">
+            <p>Calensync © 2023</p>
+          </div>
+        </div>
+        {!onlyRequired &&
+          <div className="col-12 col-sm-6 col-xxl-4">
+            <div className="row">
+              <a href={`${PUBLIC_URL}/home`}>{t("footer.home")}</a>
+            </div>
+            <div className="row">
+              <a href={`${PUBLIC_URL}/login`}>{t("footer.login")}</a>
+            </div>
+            <div className="row">
+              <a href={`${PUBLIC_URL}/dashboard`}>{t("footer.dashboard")}</a>
+            </div>
+            <div className="row">
+              <a href={`${PUBLIC_URL}/for-freelancers`}>{t("footer.for_freelancers")}</a>
+            </div>
+          </div>
+        }
       </div>
     </footer>
   );
---
+++ frontend/src/pages/ForFreelancer.tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+import { ENV, PADDLE_CLIENT_TOKEN, PUBLIC_URL } from '../utils/const';
+import Layout from '../components/Layout';
+import { PaddlePricing } from '../components/PaddlePricing';
+import { Paddle, initializePaddle } from '@paddle/paddle-js';
+import { Helmet } from "react-helmet";
+import { useTranslation } from 'react-i18next';
+
+const ForFreelancer: React.FC = () => {
+    const { t } = useTranslation();
+    const [paddle, setPaddle] = useState<Paddle | null>(null);
+
+
+    const signup = () => {
+        window.location.href = `${PUBLIC_URL}/login`;
+    }
+
+    async function setupPaddle() {
+        const paddleInstance = await initializePaddle({ environment: ENV == "production" ? "production" : "sandbox", token: PADDLE_CLIENT_TOKEN });
+        if (paddleInstance) {
+            setPaddle(paddleInstance);
+        }
+    }
+
+    useEffect(() => {
+        setupPaddle();
+    }, [])
+
+    return (
+        <Layout verify_session={false}>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>{t('title_sync_calendars')}</title>
+                <link rel="canonical" href={`https://calensync.live${PUBLIC_URL}/for-freelancer`} />
+                <meta name="description" content={t("for_freelancers.meta.description")} />
+                <meta name="og:title" content={t("for_freelancers.meta.og_title")} />
+                <meta name="og:url" content={`https://calensync.live${PUBLIC_URL}`} />
+                <meta name="og:description" content={t("for_freelancers.meta.description")} />
+            </Helmet>
+            <div className='hero'>
+                <div className="container col-xxl-8 py-5">
+                    <div className="row flex-lg-row-reverse align-items-center g-4 py-5 justify-content-center">
+                        <div className="col-10 col-sm-8 col-lg-6">
+                            <img src="hero.gif" className="d-block mx-lg-auto img-fluid hero-gif" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+                        </div>
+                        <div className="col-lg-6">
+                            <h1 className="display-5 fw-bold lh-1 mb-3">{t("for_freelancers.hero.title")}</h1>
+                            <p className="lead">
+                                {t("for_freelancers.hero.heading")}
+                            </p>
+                            <div className="d-grid gap-2 d-md-flex justify-content-md-start mt-4">
+                                <button type="button" className="btn btn-primary btn-lg px-4 me-md-2" onClick={signup}>{t("home.hero.cta")}</button>
+                            </div>
+                            <p className='mt-1 small'>{t("free_trial")}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container col-xxl-8 gy-4'>
+                <div className="row g-lg-4 mt-4 mt-lg-3 mb-lg-3 pt-lg-3 pb-lg-2 row-cols-1 row-cols-lg-2">
+                    <div className="feature col py-2">
+                        <h2>{t("for_freelancers.value.1_head")}</h2>
+                        <p className='lead'>{t("for_freelancers.value.1_content")}</p>
+                    </div>
+                    <div className="feature col py-2">
+                        <h2>{t("for_freelancers.value.2_head")}</h2>
+                        <p className='lead'>{t("for_freelancers.value.2_content")}</p>
+                    </div>
+                </div>
+                <div className="row mt-0 mb-4 g-lg-4 mt-lg-3 mb-lg-2 pt-lg-3 pb-lg-5 row-cols-1 row-cols-lg-2">
+                    <div className="feature col py-2">
+                        <h2>{t("for_freelancers.value.3_head")}</h2>
+                        <p className='lead'>{t("for_freelancers.value.3_content")}</p>
+                    </div>
+                    <div className="feature col py-2">
+                        <h2>{t("for_freelancers.value.4_head")}</h2>
+                        <p className='lead'>{t("for_freelancers.value.4_content")}</p>
+                    </div>
+                </div>
+            </div>
+            <div className='hero py-5'>
+                {paddle &&
+                    <PaddlePricing paddle={paddle} isHome={true} />
+                }
+            </div>
+            <div className='container'>
+                <div className='col-xxl-8 col-12 px-4 card mt-4 pt-4 pb-2 mx-auto'>
+                    <a className='block-link' href="/blog/sync-multiple-google-calendars">
+                        <p className='text-muted small p-0 m-0'>Blog</p>
+                        <h2>{t("blog_list.sync_google_calendars.title")}</h2>
+                        <p className='text-muted'>
+                            {t("blog_list.sync_google_calendars.headline")}
+                        </p>
+                    </a>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='col-xxl-8 col-12 px-4 card mt-4 pt-4 pb-2 mx-auto'>
+                    <a className='block-link' href="blog/avoid-calendly-conflicts">
+                        <p className='text-muted small p-0 m-0'>Blog</p>
+                        <h2>{t("blog_list.avoid_calendly_conflicts.title")}</h2>
+                        <p className='text-muted'>
+                            {t("blog_list.avoid_calendly_conflicts.headline")}
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </Layout >
+    );
+};
+
+export default ForFreelancer;
