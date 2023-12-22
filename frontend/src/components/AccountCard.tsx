1a5de67c44aa12599d1cdaed12f92e3eaece2424
dominusmi <barp.edoardo@gmail.com>
2023-12-22 17:47:40 +0100

    Remove refresh button

--- frontend/src/components/AccountCard.tsx
+++ frontend/src/components/AccountCard.tsx
@@ -50,18 +50,8 @@ const AccountCard: React.FC<{ account: Account }> = ({ account }) => {
         <div className="container-sm card my-xs-2 my-sm-4 py-4 shadow-sm rounded border-0 template account-row">
             <div className="row mx-xs-0 mx-sm-2">
                 <div className="row my-2">
-                    <div className="col-10">
-                        <h5 className="mb-1 email-value">{account.key}</h5>
-                    </div>
-                    <div className="col-2">
-                        <button className="btn btn-lg refresh-account"><svg xmlns="http://www.w3.org/2000/svg" width="20"
-                            height="20" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                            <path fillRule="evenodd"
-                                d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
-                            <path
-                                d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
-                        </svg>
-                        </button>
+                    <div className="col-12">
+                        <h6 className="mb-1 email-value">{account.key}<span className='text-muted small'>'s Google calendars</span></h6>
                     </div>
                 </div>
                 {calendars && calendars.map((calendar) => (
