34a7f30866cfb37c67c2689881f45039c669d08e
dominusmi <barp.edoardo@gmail.com>
2023-12-23 11:00:08 +0100

    Add tooltip

--- frontend/src/components/AccountCalendar.tsx
+++ frontend/src/components/AccountCalendar.tsx
@@ -5,8 +5,9 @@ import { getLocalSession } from '../utils/session';
 import axios from 'axios';
 import { createToast } from './Toast';
 import { MessageKind } from '../utils/common';
+import { Tooltip } from 'react-tooltip'
 
-const AccountCalendar: React.FC<{calendar: Calendar}> = ({calendar}) => {
+const AccountCalendar: React.FC<{ calendar: Calendar }> = ({ calendar }) => {
     const [isChecked, setChecked] = useState(calendar.active);
 
     const updateChecked = async () => {
@@ -25,10 +26,10 @@ const AccountCalendar: React.FC<{calendar: Calendar}> = ({calendar}) => {
                     },
                 );
                 calendar.active = !isChecked
-                if(calendar.active){
+                if (calendar.active) {
                     createToast("The synchronization has begun! You should see the events within 30 seconds", MessageKind.Success);
                 }
-                else{
+                else {
                     createToast("Succesfully un-synced calendar", MessageKind.Success);
                 }
                 setChecked(!isChecked);
@@ -45,20 +46,26 @@ const AccountCalendar: React.FC<{calendar: Calendar}> = ({calendar}) => {
     useEffect(() => {
         // Update the local state when the calendar prop changes
         setChecked(calendar.active);
-      }, [calendar.active]);
+    }, [calendar.active]);
 
     return (
-        
-        <div key={calendar.uuid} className="row row-cols-12 row-cols-xs-9 calendar-row my-1 overflow-auto">
-            <div className="col-md-1 d-flex">
+
+        <div key={calendar.uuid} className="row row-cols-12 row-cols-xs-9 calendar-row my-1 overflow-auto text-break">
+            <div className="col px-0 d-flex">
                 <div className='form-check form-switch'>
-                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"
-                    checked={calendar.active}
-                    onChange={updateChecked}
-                />
+                    <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"
+                        checked={calendar.active}
+                        onChange={updateChecked}
+                    />
                 </div>
-                <label className="form-check-label">{calendar.name}</label>
+                <label className="form-check-label xs-small">{calendar.name}</label>
+                {calendar.name.includes("group.v.calendar.google.com") &&
+                    <div>
+                        <p className='mx-2 my-0 google-calendar'>ℹ️</p>
+                    </div>
+                }
             </div>
+            <Tooltip anchorSelect='.google-calendar'>This is a Google generated calendar. <br></br>It is read-only, so events can be copied from it, but not to it.</Tooltip>
         </div>
     );
     return (
