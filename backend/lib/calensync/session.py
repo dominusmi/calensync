bb9e6fa0f8d433a0947a05a4f70c0b352fbcc508
dominusmi <barp.edoardo@gmail.com>
2024-01-10 11:02:37 +0100

    Add docstring, test, and fix missing tos (shouldn't be possible with new system but better safe than sorry)

--- backend/lib/calensync/session.py
+++ backend/lib/calensync/session.py
@@ -3,6 +3,10 @@ from calensync.utils import utcnow, prefetch_get_or_none
 
 
 def create_session_and_user(state_db: OAuthState, email):
+    """
+    Creates a session for user, based on the state. If email is not defined,
+    then creates a new user with the provided email
+    """
     email_db = prefetch_get_or_none(
         EmailDB.select().where(EmailDB.email == email),
         User.select()
@@ -13,6 +17,9 @@ def create_session_and_user(state_db: OAuthState, email):
         EmailDB(email=email, user=user_db).save_new()
     else:
         user_db = email_db.user
+        if user_db.tos is None and state_db.tos is not None:
+            user_db.tos = utcnow()
+            user_db.save()
 
     Session(session_id=state_db.session_id, user=user_db).save()
     state_db.delete_instance()
