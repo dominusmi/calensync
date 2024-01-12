9a4db35ba9310a4c5d6b4bf5af2845ea58f5e7b0
dominusmi <barp.edoardo@gmail.com>
2024-01-12 19:33:52 +0100

    Make accounts and calendar appear in reverse between source and destination

--- frontend/src/components/SyncRuleDraftRow.tsx
+++ frontend/src/components/SyncRuleDraftRow.tsx
@@ -5,24 +5,24 @@ import { MessageKind, refactorCalendarName, refreshPage, sleep } from '../utils/
 import API from '../utils/const';
 import LoadingOverlay from './LoadingOverlay';
 
-const SyncRuleDraftRow: React.FC<{ accounts: Account[], state: boolean, setState: (x: boolean) => void}> = ({ accounts, state, setState }) => {
+const SyncRuleDraftRow: React.FC<{ accounts: Account[], state: boolean, setState: (x: boolean) => void }> = ({ accounts, state, setState }) => {
     const sourceRef = useRef<HTMLSelectElement | null>(null);
     const destinationRef = useRef<HTMLSelectElement | null>(null);
     const busyRef = useRef<HTMLInputElement | null>(null);
     const [loading, setLoading] = useState(false);
 
-    async function createSyncRule(){
+    async function createSyncRule() {
         setLoading(true);
         const source = sourceRef.current?.value;
         const destination = destinationRef.current?.value;
         const markAsPrivate = busyRef.current?.checked || false;
 
         try {
-            if(source == null || destination == null){
+            if (source == null || destination == null) {
                 createToast("Invalid source or destination", MessageKind.Error);
                 return
             }
-            else if(source == destination){
+            else if (source == destination) {
                 createToast("Source and destination must be different", MessageKind.Error);
                 return
             }
@@ -30,7 +30,7 @@ const SyncRuleDraftRow: React.FC<{ accounts: Account[], state: boolean, setState
                 method: 'POST',
                 credentials: 'include',
                 headers: {
-                    'Content-Type': 'application/json', 
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
                     source_calendar_id: source,
@@ -40,9 +40,9 @@ const SyncRuleDraftRow: React.FC<{ accounts: Account[], state: boolean, setState
             });
 
             const body = await response.json();
-            if(!response.ok){
+            if (!response.ok) {
                 let msg = "Error while creating sync rule";
-                if(body.detail != null){
+                if (body.detail != null) {
                     msg = body.detail
                 }
                 createToast(msg, MessageKind.Error);
@@ -56,14 +56,14 @@ const SyncRuleDraftRow: React.FC<{ accounts: Account[], state: boolean, setState
         }
     }
 
-    if(!state){
-      return (<></>)
+    if (!state) {
+        return (<></>)
     }
 
     return (
         <div className='' key='test'>
             <div className='my-3 card px-sm-2 px-1'>
-                { loading && <LoadingOverlay/> }
+                {loading && <LoadingOverlay />}
                 <div className="d-flex-lg align-items-center my-2 my-lg-2">
                     <div className='row my-md-2'><span className="badge bg-secondary text-light ms-3 col-3 col-lg-1" style={{ maxWidth: "94px" }}>Draft</span></div>
                     <div className="btn-group pe-lg-2 col-12 col-lg-3 form-floating my-lg-0 my-2">
@@ -85,12 +85,12 @@ const SyncRuleDraftRow: React.FC<{ accounts: Account[], state: boolean, setState
                     </div>
                     <div className="btn-group pe-lg-2 col-12 col-lg-3 form-floating my-lg-0 my-2">
                         <select className="form-select" aria-label="Floating label select example" ref={destinationRef} >
-                            {accounts.map((a) => {
+                            {accounts.reverse().map((a) => {
                                 return (
                                     <React.Fragment key={`${a.uuid}-destination-fragment`}>
                                         <option key={`${a.uuid}-destination`} disabled={true}>{a.key}</option>
                                         {
-                                            a.calendars?.map(c =>
+                                            a.calendars?.reverse().map(c =>
                                                 <option key={`${c.uuid}-destination`} value={c.uuid}>{refactorCalendarName(c.name)}</option>
                                             )}
                                     </React.Fragment>
@@ -102,7 +102,7 @@ const SyncRuleDraftRow: React.FC<{ accounts: Account[], state: boolean, setState
                     </div>
                     <div className="btn-group pe-lg-2 col-12 col-lg-4 my-lg-0 my-2" role="group" aria-label="Third group">
                         <div className="me-2 form-check px-4">
-                            <input className="form-check-input" type="checkbox" value="" readOnly={true} ref={busyRef}/>
+                            <input className="form-check-input" type="checkbox" value="" readOnly={true} ref={busyRef} />
                             <label className="form-check-label text-start">
                                 Replace event name with "Busy"
                             </label>
