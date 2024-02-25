eac1a70b46c0e4f29d1676edf6d0787b69fea4c3
dominusmi <barp.edoardo@gmail.com>
2024-02-25 12:19:05 +0000

    Add calensync vs notion link in footer

--- frontend/src/utils/blog.tsx
+++ frontend/src/utils/blog.tsx
@@ -1,5 +1,6 @@
-import { BlogLanguageWrapper, RouteWrapper } from "reactyll";
+import { BlogLanguageWrapper, BlogProperties, RouteWrapper } from "reactyll";
 import { ExtraProperties } from "../components/BlogTemplate";
+import { blogs } from "../_blog/routes";
 
 export function sortBlogsByDate(blogs: RouteWrapper<ExtraProperties>): BlogLanguageWrapper<ExtraProperties>[] {
     // Convert the blogs object to an array of entries
@@ -14,4 +15,13 @@ export function sortBlogsByDate(blogs: RouteWrapper<ExtraProperties>): BlogLangu
 
     // Extract and return the sorted values (list of objects with key2 as keys)
     return blogEntries.map(([key, value]) => value);
+}
+
+export function getBlogByLanguage(wrapper: BlogLanguageWrapper<ExtraProperties>, language: string | undefined): BlogProperties & ExtraProperties {
+    // Convert the blogs object to an array of entries
+    if(language == null || !(language in wrapper)){
+        return wrapper["en"]
+    }else {
+        return wrapper[language]
+    }
 }
