454fd92140d8e4bba17435cc0fe393431d129f0c
dominusmi <barp.edoardo@gmail.com>
2024-04-28 17:03:16 +0200

    Add readonly field

--- backend/lib/calensync/database/model.py
+++ backend/lib/calensync/database/model.py
@@ -129,6 +129,7 @@ class Calendar(UUIDBaseModel):
     last_processed = DateTimeField(default=utcnow)
     paused = DateTimeField(null=True, default=None)
     paused_reason = CharField(null=True, default=None)
+    readonly = peewee.BooleanField(default=False)
 
     @property
     def friendly_name(self):
