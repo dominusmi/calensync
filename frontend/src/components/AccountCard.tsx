7f27141191092d567034aecb62315c0f494275b3
dominusmi <barp.edoardo@gmail.com>
2024-05-07 18:55:07 +0200

    Add front-end re-sync calendars

--- frontend/src/components/AccountCard.tsx
+++ frontend/src/components/AccountCard.tsx
@@ -4,6 +4,11 @@ import { Accordion } from 'react-bootstrap';
 import { Tooltip } from 'react-tooltip'
 import LoadingOverlay from './LoadingOverlay';
 import { useTranslation } from 'react-i18next';
+import { ActionIcon, Flex, Button } from '@mantine/core';
+import { IconReload } from '@tabler/icons-react';
+import { handleApiError } from '../utils/app';
+import { createToast } from './Toast';
+import { MessageKind } from '../utils/common';
 
 export interface Calendar {
     uuid: string;
@@ -39,6 +44,26 @@ const AccountCard: React.FC<{ account: Account }> = ({ account }) => {
         }
     }
 
+    const refreshCalendarEvents = async (calendar_uuid: string) => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(
+                `${API}/calendars/${calendar_uuid}/resync`,
+                {
+                    credentials: 'include',
+                    method: 'POST'
+                }
+            )
+            if (response.ok) {
+                createToast("All rules that use this calendar are going to be refreshed.", MessageKind.Info);
+            } else{
+                handleApiError(response, t)
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <div className="container-sm card my-1 my-sm-2 py-3 shadow-sm rounded border-0 template account-row">
             {isLoading &&
@@ -52,22 +77,27 @@ const AccountCard: React.FC<{ account: Account }> = ({ account }) => {
                                 <h6 key={`${account.uuid}-title`} className="mb-1 email-value">
                                     {account.key}<span className='text-muted small'> 's Google calendars</span>
                                 </h6>
-                                <button className='btn btn-primary ms-4' onClick={refreshCalendars}>
-                                    {t("dashboard.refresh-calendars")}
-                                </button>
                             </div>
                         </Accordion.Header>
                         <Accordion.Body>
-                            <>
+                            <Flex direction='column'>
+                                <div className='mb-3'>
+                                    <button className='btn btn-primary ' onClick={refreshCalendars}>
+                                        {t("dashboard.refresh-calendars")}
+                                    </button>
+                                </div>
                                 {account && account.calendars && account.calendars.map((calendar) => (
-                                    <div className='d-flex' key={`${calendar.uuid}-div`}>
+                                    <Flex direction={{base: 'column', md: 'row'}} key={`${calendar.uuid}-div`} align='start' mb='md' justify={{base: 'left', md: 'space-between'}}>
                                         <label className="form-check-label xs-small">{calendar.name.replace("@group.v.calendar.google.com", "")}</label>
                                         {calendar.name.includes("group.v.calendar.google.com") &&
                                             <div className='mx-2 my-0 google-calendar-tooltip'>ℹ️</div>
                                         }
-                                    </div>
+                                        <button className='btn btn-primary' onClick={() => refreshCalendarEvents(calendar.uuid)} >
+                                            <IconReload stroke={1} /> {t("dashboard.refresh-events")}
+                                        </button>
+                                    </Flex>
                                 ))}
-                            </>
+                            </Flex>
 
                         </Accordion.Body>
                     </Accordion.Item>
